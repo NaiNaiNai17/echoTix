ef90dad701e2e1af27425e6a9540447c6355e8fb
Lars Nemeth <lars.nemeth@posteo.de>
2022-01-24 15:12:27 +0100

    added frontfolders and files

---
+++ frontend/src/components/Evaluation/FinancialEvaluation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FinancialEvaluation = () => {
+  return <div></div>;
+};
+
+export default FinancialEvaluation;
---
+++ frontend/src/pages/Admin/ListUsers.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListUsers = () => {
+  return <div></div>;
+};
+
+export default ListUsers;
