0bb34901f8117bedfc140e2a16d77cc7ce46f518
Lars Nemeth <lars.nemeth@posteo.de>
2022-01-25 13:43:19 +0100

    started to set-up styled-components and general things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import MainRouter from './hoc/MainRouter';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello echoTix</h1>
+      <MainRouter />
     </div>
   );
 }
