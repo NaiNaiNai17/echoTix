8fc6359863e72263e6576fe1bda5fc770c598113
Lars Nemeth <lars.nemeth@posteo.de>
2022-01-27 15:45:54 +0100

    alles auf Anfang

---
+++ frontend/src/components/Hero/Hero-Parkplatz.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useState } from 'react';
+import { sliderItems } from '../../data';
+
+import {
+  Container,
+  Wrapper,
+  ImgContainer,
+  // InfoContainer,
+  // EventTitle,
+  // ShowDescription,
+  // Button,
+  Arrow,
+  Image,
+  Slide,
+} from '../../components/styles/Hero.styled';
+import { ArrowLeft, ArrowRight } from '@material-ui/icons';
+
+const Hero = () => {
+  //* Slider Function
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick('left')}>
+        <ArrowLeft />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            {/* <InfoContainer>
+              <EventTitle>{item.event}</EventTitle>
+              <ShowDescription>{item.description}</ShowDescription>
+              <Button>Buy Now</Button>
+            </InfoContainer> */}
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick('right')}>
+        <ArrowRight />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Hero;
--- frontend/src/components/styles/Hero.styled.js
+++ frontend/src/components/styles/Hero.styled.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components/macro';
 
-//* HERO / SLIDER
-
 export const Container = styled.div.attrs(() => ({
   className: 'Container',
 }))`
   width: 100%;
-  height: 100vh;
+  height: 80vh;
   display: flex;
   justify-content: center;
   background-color: #cad2c5;
@@ -14,72 +12,21 @@ export const Container = styled.div.attrs(() => ({
   overflow: hidden;
 `;
 
-//* Slider Section - Image Container
-
-export const Wrapper = styled.div.attrs(() => ({
-  className: 'Wrapper',
+export const ImageContainer = styled.div.attrs(() => ({
+  className: 'ImageContainer',
 }))`
-  height: 100%;
-  display: flex;
-  transform: translateX(${(props) => props.slideIndex * 100}vw);
-`;
-
-export const Slide = styled.div.attrs(() => ({
-  className: 'Slide',
-}))`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-conten: center;
-`;
-
-export const ImgContainer = styled.div.attrs(() => ({
-  className: 'ImgContainer',
-}))`
-  height: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
+  margin: 10px;
+  transform: translateX(${(props) => props.slideIndex * 100}vw);
 `;
 
 export const Image = styled.img.attrs(() => ({
   className: 'Image',
 }))`
-  height: 50%;
+  height: 80%;
 `;
-
-//* Info Container
-
-export const InfoContainer = styled.div.attrs(() => ({
-  className: 'InfoContainer',
-}))`
-  width: 50%;
-  padding: 50px;
-`;
-
-export const EventTitle = styled.h1.attrs(() => ({
-  className: 'EventTitle',
-}))`
-  font-size: 50px;
-`;
-
-export const ShowDescription = styled.p.attrs(() => ({
-  className: 'ShowDescription',
-}))`
-  margin: 10px 0px;
-  font-size: 10px;
-  font-weight: 500px;
-  letter-spacing: 3px;
-`;
-
-export const Button = styled.button.attrs(() => ({
-  className: 'Button',
-}))`
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-//* Arrows for Slider
 
 export const Arrow = styled.div.attrs(() => ({ className: 'Arrow' }))`
   width: 50px;
