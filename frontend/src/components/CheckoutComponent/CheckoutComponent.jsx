50826ef2fbd34e308d900e8a12dd5c725673e456
Lars Nemeth <lars.nemeth@posteo.de>
2022-01-31 11:48:42 +0100

    CheckoutComponent finished (so far)

--- frontend/src/components/CheckoutComponent/CheckoutComponent.jsx
+++ frontend/src/components/CheckoutComponent/CheckoutComponent.jsx
@@ -8,6 +8,7 @@ import Remove from '@material-ui/icons/RemoveCircleOutline';
 
 import {
   CheckoutContainer,
+  WrapAll,
   CheckoutWrapper,
   CartTitle,
   CheckoutTop,
@@ -15,7 +16,6 @@ import {
   TopButton,
   TopText,
   Info,
-  Summary,
   Ticket,
   Details,
   City,
@@ -29,14 +29,20 @@ import {
   TicketAmountCountainer,
   TicketAmount,
   TicketPrice,
+  Hr,
+  Summary,
+  SummaryTitle,
+  SummaryItem,
+  SummaryItemText,
+  SummaryItemPrice,
+  SummaryButton,
 } from '../../components/styles/Checkout.styled';
-import { StylesContext } from '@material-ui/styles';
 
 const CheckoutComponent = () => {
   return (
     <CheckoutContainer>
       <Navbar />
-      <div >
+      <WrapAll>
         <CheckoutWrapper>
           <CartTitle>Shopping Cart</CartTitle>
           <CheckoutTop>
@@ -75,11 +81,60 @@ const CheckoutComponent = () => {
                   <TicketPrice>EUR 30</TicketPrice>
                 </PriceDetail>
               </Ticket>
+              <Hr />
+              <Ticket>
+                <TicketDetail>
+                  <Image src="https://i.discogs.com/UkAHVId2CtYN8zmwzjFF3k5KLFTwQuAYfJaSWR-7RUY/rs:fit/g:sm/q:90/h:548/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9SLTIz/NDgxODAtMTUwMjEy/MDkxMy01NjgxLmpw/ZWc.jpeg" />
+                  <Details>
+                    <City>Berlin</City>
+                    <EventName>Damien Jurado</EventName>
+                    <Venue>
+                      <b>Venue:</b>
+                      <div>Astra</div>
+                    </Venue>
+                    <Date>
+                      <b>Date</b>
+                      <div>22.11.2022</div>
+                    </Date>
+                    <EventID>
+                      <b>EventID</b>
+                      <div></div>
+                    </EventID>
+                  </Details>
+                </TicketDetail>
+                <PriceDetail>
+                  <TicketAmountCountainer>
+                    <Add style={{ width: '75px', height: '75px' }} />
+                    <TicketAmount>2</TicketAmount>
+                    <Remove style={{ width: '75px', height: '75px' }} />
+                  </TicketAmountCountainer>
+                  <TicketPrice>EUR 20</TicketPrice>
+                </PriceDetail>
+              </Ticket>
             </Info>
-            <Summary>Summary</Summary>
+            <Summary>
+              <SummaryTitle>Order Summary</SummaryTitle>
+              <SummaryItem>
+                <SummaryItemText>Subtotal</SummaryItemText>
+                <SummaryItemPrice>EUR 50</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Donation</SummaryItemText>
+                <SummaryItemPrice>EUR 20</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Tax (19% MwST)</SummaryItemText>
+                <SummaryItemPrice>EUR 5</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem type="total">
+                <SummaryItemText>Total</SummaryItemText>
+                <SummaryItemPrice>EUR 50</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryButton>Checkout Now</SummaryButton>
+            </Summary>
           </CheckoutBottom>
         </CheckoutWrapper>
-      </div>
+      </WrapAll>
       <Footer />
     </CheckoutContainer>
   );
