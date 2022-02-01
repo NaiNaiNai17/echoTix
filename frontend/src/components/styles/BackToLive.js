5e5d4959d2ffb2c68364da41ff7be3c4f8a32c14
Lars Nemeth <lars.nemeth@posteo.de>
2022-02-01 11:39:28 +0100

    started to shape back to live

---
+++ frontend/src/components/styles/BackToLive.js
@@ -0,0 +1,12 @@
+
+import styled from 'styled-components/macro';
+
+
+
+export const ImageContainer = styled.h1.attrs(() => ({
+  className: 'ImageContainer',
+}))``;
+
+export const Image = styled.img.attrs(() => ({
+  className: 'Image',
+}))``;
