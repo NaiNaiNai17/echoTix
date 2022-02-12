a5feaefd2b765eff32a77639541cbafe3d78944b
Lars Nemeth <lars.nemeth@posteo.de>
2022-02-12 05:52:12 +0100

    herosectionbeautiful

--- frontend/src/components/styles/Hero.styled.js
+++ frontend/src/components/styles/Hero.styled.js
@@ -4,16 +4,97 @@ export const HeroImageContainer = styled.div.attrs(() => ({
   className: 'HeroImageContainer',
 }))`
   position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-content: space-between;
 `;
 
 export const HeroImage = styled.img.attrs(() => ({
   className: 'HeroImage',
 }))`
   width: 100%;
-  height: 100%;
+  height: 20rem;
   object-fit: cover;
   display: block;
+
   :hover {
     opacity: 1;
   }
+`;
+
+export const InfoContainer = styled.div.attrs(() => ({
+  className: 'InfoContainer',
+}))`
+  padding-left: 30px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
+  padding: 50px;
+  opacity: 0;
+  justify-content: flex-start;
+  flex-direction: column;
+
+  ${'' /* opacity: 0; */}
+
+  :hover {
+    opacity: 1;
+    background: rgba(0, 0, 0, 0.6);
+
+    p {
+      background: linear-gradient(
+        90deg,
+        rgba(2, 0, 36, 1) 0%,
+        rgba(38, 38, 38, 1) 3%
+      );
+
+      font-size: smaller;
+      line-height: 1.2rem;
+      padding: 20px;
+      color: #fff;
+      border-radius: 20px;
+    }
+  }
+
+  p {
+    color: #fff;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    opacity: 1;
+  }
+`;
+
+export const InfoWrapper = styled.div.attrs(() => ({
+  className: 'InfoWrapper',
+}))`
+  height: 5rem;
+  opacity: 0.5;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  background-color: background: rgba(233, 03, 30, 0.8);
+`;
+
+export const Title = styled.h1.attrs(() => ({
+  className: 'Title',
+}))`
+  color: #fff;
+  font-size: 3rem;
+  font-weight: bold;
+  font-family: Edo;
+  text-align: start;
+`;
+
+export const Description = styled.p.attrs(() => ({
+  className: 'Description',
+}))`
+  font-size: 1rem;
+  width: 30rem;
+  text-align: start;
 `;
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -3,18 +3,24 @@ export const sliderItems = [
     id: 1,
     img: 'https://www.mercedes-benz-arena-berlin.de/assets/img/BonIver_Header_MBA-6e0d0ae303.jpg',
     name: 'Bon Iver',
-    description: 'Future Folk',
+    date: 'October 31, 2022',
+    description:
+      'With his 2007 debut album, Justin Vernon demonstrated the soul-cleansing properties of isolating yourself from the world in a wooden shed. ',
   },
   {
     id: 2,
-    img: 'https://www.oeticket.com/obj/media/AT-eventim/teaser/artworks/2021/gregory-porter-kad-tickets-2022-artwork.jpg',
-    description: 'Chart Pop',
+    img: 'https://www.langweiledich.net/wp-content/uploads/2017/11/Rhye-Taste.jpg',
+
+    name: 'Rhye',
+    description:
+      'Gefühlvoll arrangierte Piano-Tracks, organische Percussion, schwirrende Synthies und eine intensive Stimme',
   },
   {
     id: 3,
     img: 'https://www.eventim.de/obj/media/DE-eventim/teaser/artworks/2021/curtis-harding-tickets-artwork.jpg',
     name: 'Curtis Harding',
-    description: 'Funk Rock',
+    description:
+      'Hardings style was born in Michigan and bred on the road, a restless childhood spent singing Gospel alongside an evangelizing mother',
   },
 ];
 
