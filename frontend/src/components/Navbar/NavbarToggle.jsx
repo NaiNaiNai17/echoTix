f718fbf359434a10d8413b2ae1cb8493a04b2f4a
Lars Nemeth <lars.nemeth@posteo.de>
2022-02-23 16:36:24 +0100

    done

--- frontend/src/components/Navbar/NavbarToggle.jsx
+++ frontend/src/components/Navbar/NavbarToggle.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { ToggleContainer } from '../styles/Navbar.styled';
 
 const NavbarToggle = ({ open }) => {
+  const navigate = useNavigate();
+  function goHome() {
+    navigate('/');
+  }
+
+  function login() {
+    navigate('/login');
+  }
+
+  function register() {
+    navigate('/register');
+  }
+
+  function shoppingCart() {
+    navigate('/shoppingcart');
+  }
+
   return (
     <>
       {open ? (
         <ToggleContainer open={open}>
           <ul>
-            <li>Home</li>
-            <li>Login</li>
-            <li>Shopping Cart</li>
-            <li>Impressum</li>
+            <a href="">
+              <li onClick={goHome} style={{ cursor: 'pointer' }}>
+                Home
+              </li>
+            </a>
+            <a href="">
+              <li onClick={register} style={{ cursor: 'pointer' }}>
+                Login
+              </li>
+            </a>
+            <a href="">
+              <li onClick={register} style={{ cursor: 'pointer' }}>
+                Register
+              </li>
+            </a>
+
+            <a href="">
+              <li>Shopping Cart</li>
+            </a>
+            <a href="">
+              <li>Impressum</li>
+            </a>
           </ul>
         </ToggleContainer>
       ) : null}
