e88dd0549014147bd8f8f5791639cf6a812c8427
NaiNaiNai17 <fowler.naimah@gmail.com>
2022-02-25 16:12:13 +0100

    added city to buy page

--- frontend/src/components/Buttons/Buy.jsx
+++ frontend/src/components/Buttons/Buy.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom';
 
 import { BuyButton } from '../../components/styles/Buttons/Buy.styled';
 
-const Buy = ({id, price, qty, venue, img, date, showName}) => {
+const Buy = ({id, price, qty, venue, img, date, showName, city}) => {
   const navigate = useNavigate();
 
   const onBuyHandler = async () => { 
    
     
     const basketContents = JSON.parse(sessionStorage.getItem('basket')) || [];
-    basketContents.push({id, price, qty, venue, img, date, showName});
+    basketContents.push({id, price, qty, venue, img, date, showName, city});
     sessionStorage.setItem('basket', JSON.stringify(basketContents));
     //const response = await axios.post('/shows/checkout', { body: {eventID, ticketAmount, customerID} })
     navigate('/shoppingcart');
